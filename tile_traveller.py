280c4bed84c3eba0aeb518d39d65153017f6ce0b
Dilja Sigurdardottir <diljas19@ru.is>
2019-09-17 14:06:29 +0000

    second commit

---
+++ tile_traveller.py
@@ -0,0 +1,21 @@
+# 1. A game where a player starts in tile (1,1)
+# 2. Each move the player selects the program should print the players travel options
+# 3. Before the enters a direction he can see what directions he can choose
+# 4. Directions North (up), South (down), West (left), East (right)
+# 6. Player can only go where he wants if the wall is open
+# 7. If the wall is closed then it should print out: "not a valid direction!" and the player can try again
+# 8. When the player gets to tile (3,1) then the program should print out victory!
+
+
+def directions(N, S, E , W):
+
+
+def tiles(x, y): 
+
+
+START_X = 1
+START_Y = 1
+
+direction = input("Direction: ")
+
+
