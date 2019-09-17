ded9fe26d29416d58fc8e58d6ceb5a67783f3932
Sunna Run Thorarinsdottir <sunnat16@ru.is>
2019-09-17 14:28:49 +0000

    first def

--- tile_traveller.py
+++ tile_traveller.py
@@ -6,16 +6,49 @@
 # 7. If the wall is closed then it should print out: "not a valid direction!" and the player can try again
 # 8. When the player gets to tile (3,1) then the program should print out victory!
 
-
-def directions(N, S, E , W):
-
-
-def tiles(x, y): 
-
-
 START_X = 1
 START_Y = 1
 
-direction = input("Direction: ")
+tile = (START_X, START_Y)
+
+valid_directions = ""
+
+def directions(N, S, E ,W):
+    if 
+
+def tiles(x, y): 
+    X_current = 1
+    Y_current = 1
+    if tile == (1, 1):
+        valid direction = (N)
+    elif tile == (1, 2):
+        valid_directions = (N, E, S)
+    elif tile == (2, 2):
+        valid_directions = (S, W)
+    elif tile == (2, 1):
+        valid_directions = (N)
+    elif tile == (1, 3):
+        valid_directions = (S, E)
+    elif tile == (2, 3):
+        valid_directions = (W, E)
+    elif tile == (3, 3):
+        valid_directions = (W, S)
+    elif tile == (3, 2):
+        valid_directions = (N, S)
+    else:
+        print("Victory!")
+        break
+
+
+def invalid():
+
+def print_directions(START_X, START_Y):
+    print("You can travel: ", valid_directions)
+
+
+
+direction = 
+print("You can travel: ", direction)
+N_S_W_E = input("Direction: ")
 
 
